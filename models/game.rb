c14927e8180c97b84771f5d74ea6a6d4ab7ef679
John Michaels <johnrmichaelsjr@gmail.com>
2017-08-26 22:46:41 +0200

    Finishing up the initial game rules/logic and UI
    Time for play testing!

--- models/game.rb
+++ models/game.rb
@@ -3,7 +3,11 @@ class Game
               :player_hand,
               :dealer_hand,
               :ui,
-              :hl
+              :hl,
+              :result,
+              :win_count,
+              :loss_count,
+              :tie_count
 
   def initialize
     @deck = Deck.new
@@ -11,25 +15,34 @@ class Game
     @dealer_hand = Hand.new
     @ui = UI.new
     @hl = HighLine.new
+    @result = nil
+    @win_count = 0
+    @loss_count = 0
+    @tie_count = 0
   end
 
   def run
     intro
     deal
-    #answer = cli.ask('What do you think?')
-    #puts "You have answered #{answer}."
+    check_for_naturals
+    player_move
+  end
+
+  def replay
+    @deck = Deck.new
+    @player_hand = Hand.new
+    @dealer_hand = Hand.new
+    @result = nil
+
+    deal
+    check_for_naturals
+    player_move
   end
 
   def intro
-    ui.clear
-    ui.title([
-      "Welcome to BlackJack!",
-      "A game of chance and skill."
-    ])
-    ui.space(2)
+    ui.render('intro', self)
     hl.ask("Press ENTER to begin...")
-    ui.space(2)
-    ui.clear
+    ui.loading_animation
   end
 
   def deal
@@ -37,41 +50,138 @@ class Game
       @player_hand.cards << deck.draw_a_card
       @dealer_hand.cards << deck.draw_a_card
     end
-
-    puts
-
-    ui.print_hands(@dealer_hand, @player_hand)
-
-    puts "\nDo you hit or stay?"
-    puts "h - Hit"
-    puts "s - Stay \n"
   end
 
+  def check_for_naturals
+    if @player_hand.points == 21 && @dealer_hand.points == 21
+      end_game('tie_win')
+    elsif @player_hand.points == 21
+      end_game('player_win')
+    elsif @dealer_hand.points == 21
+      end_game('dealer_win')
+    end
+  end
 
+  def player_move
+    ui.render('player_move', self)
 
-=begin
-puts "Dealing a new game..."
-UI.loading_animation
-deal!
-puts
-@player_hand.cards.each do |card|
-  UI.print_card(card)
-  sleep 0.5
-end
-puts '-' * 50
-puts "You have: #{@player_hand.points} points.\n"
-puts '-' * 50
+    hl.choose do |menu|
+      menu.prompt = "Do you 'hit' or 'stand'?"
+      menu.choice(:hit) { player_hit }
+      menu.choice(:stand) { player_stand }
+    end
+  end
 
-puts "\nDo you hit or stay?"
-puts "h - Hit"
-puts "s - Stay \n"
-print ":> "
-response = STDIN.gets.chomp
-puts response
-    UI.space(3)
-    5.times{ |i| print "\r#{5 - i} Let's begin"; sleep 1 }
-    UI.space(1)
-=end
-#cli = HighLine.new
+  def player_hit
+    ui.say "You hit."
+    @player_hand.cards << deck.draw_a_card
 
+    sleep 0.5
+
+    if @player_hand.points > 21
+      end_game('player_bust')
+    elsif @player_hand.points == 21
+      ui.say "You have 21 points."
+      ui.say "Dealer's move."
+      sleep 1
+      dealer_move
+    else
+      player_move
+    end
+  end
+
+  def player_stand
+    ui.say "You stand."
+    dealer_move
+  end
+
+  def check_for_player_bust
+    if @player_hand.points > 21
+      end_game('player_bust')
+    end
+  end
+
+  def dealer_move
+    ui.render('dealer_move', self)
+
+    if @dealer_hand.points < 17 || @dealer_hand.points < @player_hand.points
+      @dealer_hand.cards << @deck.draw_a_card
+      ui.say "The Dealer hits."
+      sleep 2
+      if @dealer_hand.points > 21
+        end_game('dealer_bust')
+      else
+        dealer_move
+      end
+    else
+      ui.say "The Dealer stands."
+      sleep 2
+      check_for_game_end
+    end
+
+  end
+
+  def check_for_game_end
+    if @player_hand.points == 21 && @dealer_hand.points == 21
+      end_game('tie_win')
+    elsif @player_hand.points > 21 && @dealer_hand.points > 21
+      end_game('tie_bust')
+    elsif @player_hand.points > 21
+      end_game('player_bust')
+    elsif @dealer_hand.points > 21
+      end_game('dealer_bust')
+    elsif @player_hand.points == 21
+      end_game('player_win')
+    elsif @dealer_hand.points == 21
+      end_game('dealer_win')
+    elsif @dealer_hand.points >= @player_hand.points
+      end_game('dealer_win')
+    elsif @dealer_hand.points < @player_hand.points
+      end_game('player_win')
+    else
+      raise "This shouldn't happen"
+    end
+  end
+
+  def end_game(result)
+    @result = result
+    update_stats(result)
+    ui.render('game_end', self)
+    hl.ask("Press ENTER to play again, or CTRL+C to quit")
+    replay
+  end
+
+  def update_stats(result)
+    if ['player_win', 'dealer_bust'].include?(result)
+      add_win
+    elsif ['dealer_win', 'player_bust'].include?(result)
+      add_loss
+    else
+      add_tie
+    end
+  end
+
+  def win_percentage
+    if win_count == 0 && loss_count == 0
+      'N/A'
+    elsif win_count > 0 && loss_count == 0
+      '100'
+    else
+      # We're not including ties in this calculation
+      total_games = win_count + loss_count
+      ((win_count.to_f / total_games.to_f) * 100).round.to_s
+    end
+  end
+
+  def add_win
+    @win_count += 1
+  end
+
+  def add_loss
+    @loss_count += 1
+  end
+
+  def add_tie
+    @tie_count += 1
+  end
 end
--- models/ui.rb
+++ models/ui.rb
@@ -15,25 +15,18 @@ class UI
   end
 
   def say(msg, alignment='left')
-    if alignment == 'center'
-      puts msg.center(TERMINAL_WIDTH)
+    if alignment == 'heading'
+      msg = msg.center(TERMINAL_WIDTH - 4)
+      msg = "| #{msg}|"
+    elsif alignment == 'center'
+      msg = msg.center(TERMINAL_WIDTH)
     elsif alignment == 'right'
-      puts msg.rjust(TERMINAL_WIDTH)
+      msg = msg.rjust(TERMINAL_WIDTH)
     else
-      puts msg.ljust(TERMINAL_WIDTH)
+      msg = msg.ljust(TERMINAL_WIDTH)
     end
-  end
 
-  def title(msg)
-    # Can accept a string or an array, if multi-line title
-
-    lines = [msg].flatten
-
-    puts '#' * TERMINAL_WIDTH
-    puts
-    lines.each{ |line| say(line, 'center') }
-    puts
-    puts '#' * TERMINAL_WIDTH
+    puts msg
   end
 
   def space(number=1)
@@ -41,44 +34,49 @@ class UI
   end
 
   def loading_animation
-    puts
-    50.times do
-      print '-'
-      sleep 0.02
+    loading_msg = 'LOADING GAME: '
+    (TERMINAL_WIDTH - loading_msg.length).times do |i|
+      print "\r" + loading_msg + random_string_of_suit_emojis(i + 1)
+      sleep 0.01
     end
-    puts
-  end
-
-  def print_hands(dealer_hand, player_hand)
-    say "YOUR HAND:", 'center'
-    space(2)
-
-    print_hand(player_hand.cards)
-
-    hr
-    puts "You have: #{player_hand.points} points.\n"
-    hr
   end
 
   def hr(character='-')
-    puts character * TERMINAL_WIDTH
+    if character == 'random_suits'
+      puts random_string_of_suit_emojis(TERMINAL_WIDTH)
+    else
+      puts character * TERMINAL_WIDTH
+    end
   end
 
-  def print_hand(cards)
-    cards.each{ print "/-----\\ " }
-    space(1)
-    cards.each{ print "|     | " }
-    space(1)
-    cards.each{ |card| print print_card_value_section(card) + " " }
-    space(1)
-    cards.each{ print "|     | " }
-    space(1)
-    cards.each{ print "\\-----/ " }
-    space(2)
+  def print_hand(cards, dealer_card_face_down: false)
+    # This is a bit complicated, it's printing out the cards horizontally,
+    # which requires generating one row at a time.  So, you have to print the
+    # top 1/5 section of each card all on one line, then the 2nd 1/5 on the next.
+    rows = []
+    rows << cards.map{ "/-----\\" }.join(' ')
+    rows << cards.map{ "|     |" }.join(' ')
+    rows << print_middle_row_sections(cards, dealer_card_face_down: dealer_card_face_down).join(' ')
+    rows << cards.map{ "|     |" }.join(' ')
+    rows << cards.map{ "\\-----/" }.join(' ')
+    print rows.join("\n")
+    space
+  end
+
+  def print_middle_row_sections(cards, dealer_card_face_down:)
+    middle_row_sections = []
+    cards.each_with_index do |card, index|
+      if dealer_card_face_down && index == 0
+        middle_row_sections << "|  ?  |"
+      else
+        middle_row_sections << print_card_value_section(card)
+      end
+    end
+    middle_row_sections
   end
 
   def print_card_value_section(card)
-    "|  #{card.value}#{suit_emoji_code(card.suit)}#{card.value.length == 2 ? '' : ' ' }|"
+    "|#{card.value.length == 2 ? '' : ' ' } #{card.value}#{suit_emoji_code(card.suit)} |"
   end
 
   def ask(msg)
@@ -98,5 +96,103 @@ class UI
     else
       raise "Invalid suit name."
     end
+  end
+
+  def random_string_of_suit_emojis(how_many)
+    string = ''
+    how_many.times do |i|
+      string += suit_emoji_code(Deck::CARD_SUITS.sample)
+    end
+    string
+  end
+
+  def render(view, game)
+    clear
+    render_heading(game)
+    self.send("render_#{view}_view", game)
+  end
+
+  def render_heading(game)
+    hr('random_suits')
+    say "BlackJack", 'heading'
+    say "Wins: #{game.win_count} | Losses: #{game.loss_count} | Win %: #{game.win_percentage}", 'heading'
+    hr('random_suits')
+    space
+  end
+
+  def render_intro_view(game)
+    suit_emojis = Deck::CARD_SUITS.map{ |name| suit_emoji_code(name) }
+                                  .join('')
+
+    hr('#')
+    space
+    say 'Welcome to BlackJack!', 'heading'
+    say suit_emojis, 'heading'
+    say 'A game of chance and skill', 'heading'
+    space
+    hr('#')
+    space
+  end
+
+  def render_player_move_view(game)
+    render_hands(game, dealer_card_face_down: true)
+  end
+
+  def render_dealer_move_view(game)
+    render_hands(game, dealer_card_face_down: false)
+  end
+
+  def render_hands(game, dealer_card_face_down: true)
+    hr
+    say "YOUR HAND:", 'heading'
+    hr
+    space
+    print_hand(game.player_hand.cards)
+    space
+    say "You have: #{game.player_hand.points} points."
+    space
+    hr
+    say "DEALER'S HAND:", 'heading'
+    hr
+    space
+    print_hand(game.dealer_hand.cards, dealer_card_face_down: dealer_card_face_down)
+    space
+    unless dealer_card_face_down
+      say "The dealer has: #{game.dealer_hand.points} points."
+      space
+    end
+    hr
+  end
+
+  def render_game_end_view(game)
+    render_hands(game, dealer_card_face_down: false)
+    space(2)
+    hr '#'
+    space(1)
+    case game.result
+    when 'tie_win'
+      say "It's a tie.  Everybody wins!", 'center'
+      say "(Nobody wins.)", 'center'
+    when 'tie_lose'
+      say "Tie - everyone busted.", 'center'
+      say "(Nobody wins.)", 'center'
+    when 'player_bust'
+      say 'You busted.'
+      say 'Womp. Womp.'
+    when 'dealer_bust'
+      say 'The Dealer busted.'
+      say 'Hooray!'
+      say 'You win.'
+    when 'player_win'
+      say 'You win!'
+      say "What a lucky #{['gal', 'guy'].sample}!"
+    when 'dealer_win'
+      say "You lost :'("
+      say 'Try again?'
+    else
+      say "Game ended | #{game.result} | (IMPLEMENT ME)", 'center'
+    end
+    space(1)
+    hr '#'
   end
 end
