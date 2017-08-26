1bac23da9d105e89af2bf73afca542d938928b3a
John Michaels <johnrmichaelsjr@gmail.com>
2017-08-26 22:55:19 +0200

    Add placeholder shell scripts for install / run

---
+++ install_dependencies.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# TODO - Install ruby
+# TODO - Install bundler
+# TODO - bundle install
+# TODO - add a file to this directory saying install complete
---
+++ play_blackjack.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# TODO - Check if 'installation completed' file exists, if not:
+# TODO - Run the installer file first
+
+ruby ./run.rb
